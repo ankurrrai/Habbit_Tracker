6afdfad6f8afb1101790075f663623cf4dbc6161
ankurrrai <ankur.rai5666@gmail.com>
2023-12-29 01:11:58 +0530

    Basics setup is done

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+main().catch(function(err){
+    console.log(`Error is while connecting db : ${err}`);
+});
+
+async function main(){
+    mongoose.connect('mongodb://127.0.0.1:27017/habbit_tracker_dev');
+};
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'Error while Established the DB'));
+db.once('open',function(){
+    console.log('Connection eastablished with db');
+});
+
+module.exports=db;
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express=require('express');
 const app=express();
+const db=require('./config/mongoose');
+const expressLayouts=require('express-ejs-layouts');
+const path=require('path');
 const port=8000;
+
+
+app.use(express.urlencoded());
+app.use('/assests',express.static(path.join(__dirname,'assests')));
+app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(expressLayouts);
+app.set('layouts extractStyles',true);
+app.set('layouts extractScripts',true);
+
 
 
 app.listen(port,function(err){
